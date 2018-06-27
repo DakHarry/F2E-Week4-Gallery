33cfedac77b5758f614bd8032e7a09d1922d3dfd
DakHarry <monkey030210@gmail.com>
2018-06-27 23:35:36 +0800

    use gulp & babel & sass & pug

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,13 +2,27 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var livereload = require('gulp-livereload')
+var watch = require('gulp-watch')
+var pug = require('gulp-pug')
 
+gulp.task('default', ['sass:watch','pug:watch'])
 
-gulp.task('compressSass', () => {
-    gulp.src('./**/**.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
+gulp.task('sass', () => {
+    gulp.src('./sass/**.sass')
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/css'))
         .pipe(livereload())
+})
+
+gulp.task('pug', () => {
+    gulp.src('views/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('dist/'))
+})
+gulp.task('pug:watch', () => {
+    gulp.watch('views/*.pug',['pug'])
+})
+
+gulp.task('sass:watch', () => {
+    gulp.watch('./sass/**.sass',['sass'])
 })
